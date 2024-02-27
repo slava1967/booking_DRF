14a80ef67675a99499e8f73d7e17e1cb17daae92
Viacheslav <fx.slav21@gmail.com>
2024-02-27 17:27:08 +0300

    nearly done

--- api/models.py
+++ api/models.py
@@ -9,10 +9,16 @@ class ApiUser(AbstractUser):
 class Hotel(models.Model):
     name = models.CharField(max_length=128)
 
+    def __str__(self):
+        return f"{self.id}: {self.name}"
+
 
 class Room(models.Model):
     num = models.PositiveIntegerField()
     hotel = models.ForeignKey(Hotel, related_name="rooms", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.hotel.name}. Room num: {self.num}"
 
 
 class Booking(models.Model):
